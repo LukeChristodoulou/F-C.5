e1e77a453a60f18017174a0ad753a21aa64d341b
TotodileIsBlue <luke.c.c@live.co.uk>
2019-04-29 16:48:25 +0100

    Initial commit

---
+++ js/game(og).js
@@ -0,0 +1,52 @@
+/* function roll() {
+	var d6 = Math.floor(Math.random() * 6) + 1
+document.write(d6)  
+}
+
+function attack() {
+	player: attack * roll()
+}
+var turnMeter() {
+	10
+}
+
+var player: {
+	level: 1;
+	health: 100;
+	attack: 5;
+	defense: 4;
+	speed: 5;
+}
+var goblin: {
+	health: 60;
+	attack: 3;
+	defense: 2;
+	speed: 7;
+}
+var skeleton: {
+	health: 90;
+	attack: 4;
+	defense: 1;
+	speed: 5;
+}
+var elf: {
+	health: 120;
+	attack: 6;
+	defense: 4;
+	speed: 6;
+}
+var dwarf: {
+	health: 140;
+	attack: 4;
+	defense: 6;
+	speed: 4;
+}
+var giant: {
+	health: 200;
+	attack: 10;
+	defense: 8;
+	speed: 2;
+}
+*/
+document.write("Game")
+document.write("")
---
+++ js/game.js
@@ -0,0 +1,8 @@
+ function roll() {
+	var d6 = Math.floor(Math.random() * 6) + 1
+document.getElementById("game").innerHTML = (d6)  
+}
+
+
+document.write('"Game"')
+document.write("")
